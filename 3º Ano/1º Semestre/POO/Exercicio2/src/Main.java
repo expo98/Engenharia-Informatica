f065892c466086c363f1666c00d2e56ef4623e9d
expo98 <miguelmpereira0409@gmail.com>
2024-11-13 17:19:36 +0000

    Added  finished exercicio 2 files

--- 3º Ano/1º Semestre/POO/Exercicio2/src/Main.java
+++ 3º Ano/1º Semestre/POO/Exercicio2/src/Main.java
@@ -1,8 +1,15 @@
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        NCSLab lab = new NCSLab();
+        System.out.println("Computadores:");
+        lab.printComputadores();
 
+        System.out.println("\nComputadores com arquitetura x64:");
+        lab.print64Computadores();
+
+        System.out.println("\nConsumo de energia:");
+        lab.printConsumoEnergia();
 
     }
 }
